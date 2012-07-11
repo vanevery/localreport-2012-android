bb09db4f9d3f41149cc77abf1067231fd1b57507
vanevery <vanevery@walking-productions.com>
2012-07-11 14:34:24 -0400

    Initial checkin

---
+++ src/com/mobvcasting/localreport2012/AudioCapture.java
@@ -0,0 +1,18 @@
+package com.mobvcasting.localreport2012;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class AudioCapture extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_capture);
+    }
+
+    
+}
