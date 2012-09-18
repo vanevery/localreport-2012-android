15bd58cf0af716746b380a404aef7af99ed183a2
vanevery <vanevery@walking-productions.com>
2012-09-18 16:28:38 -0400

    Audio Call UI

--- src/com/mobvcasting/localreport2012/AudioCall.java
+++ src/com/mobvcasting/localreport2012/AudioCall.java
@@ -7,8 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class AudioCall extends Activity implements View.OnClickListener
 {
@@ -16,40 +15,13 @@ public class AudioCall extends Activity implements View.OnClickListener
 
     private TwilioPhone phone;
     Button dialButton, hangupButton, backupButton;
+    TextView callStatus;
 
     @Override
     public void onCreate(Bundle bundle)
     {
         super.onCreate(bundle);
-    	initUI();
-    }
-    
-    @Override
-    public void onResume() {
-    	super.onResume();
-    	if (phone == null) {
-    		phone = new TwilioPhone(getApplicationContext());
-        	Log.v(LOGTAG,"TwilioPhone Created");
-    	}
-    	else {
-    		Log.v(LOGTAG,"Already have a TwilioPhone");
-    	}
-    }
-    
-    @Override
-    public void onPause() {
-    	Log.v(LOGTAG,"On Pause Called");
-    	phone.disconnect();
-    	super.onPause();
-    }
-        
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-      super.onConfigurationChanged(newConfig);
-      initUI();
-    }
-    
-    public void initUI() {
+
         setContentView(R.layout.activity_audio_call);
     	
     	dialButton = (Button) findViewById(R.id.dialButton);
@@ -60,9 +32,39 @@ public class AudioCall extends Activity implements View.OnClickListener
         hangupButton.setEnabled(false);
         hangupButton.setOnClickListener(this);
         
-        backupButton = (Button)findViewById(R.id.backupButton);
-        backupButton.setOnClickListener(this);
+        callStatus = (TextView) findViewById(R.id.callStatus);
+        
+        
+        //backupButton = (Button)findViewById(R.id.backupButton);
+        //backupButton.setOnClickListener(this);
         //backupButton.setVisibility(View.GONE);
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	if (phone == null) {
+    		phone = new TwilioPhone(getApplicationContext());
+    		callStatus.setText("Ready");
+        	Log.v(LOGTAG,"TwilioPhone Created");
+    	}
+    	else {
+    		Log.v(LOGTAG,"Already have a TwilioPhone");
+    		callStatus.setText("Ready");
+    	}
+    }
+    
+    @Override
+    public void onPause() {
+    	Log.v(LOGTAG,"On Pause Called");
+    	phone.disconnect();
+    	callStatus.setText("Disconnecting");
+    	super.onPause();
+    }
+        
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+    	super.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -71,10 +73,12 @@ public class AudioCall extends Activity implements View.OnClickListener
         if (view == dialButton) {
         	dialButton.setEnabled(false);
             phone.connect();
+            callStatus.setText("Connecting");
             hangupButton.setEnabled(true);
         } else if (view == hangupButton) {
         	hangupButton.setEnabled(false);
             phone.disconnect();
+            callStatus.setText("Disconnecting");
             dialButton.setEnabled(true);
         	finish();
         } else if (view == backupButton) {
